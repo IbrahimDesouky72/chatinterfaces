72ee14aee887456e84322e5f37e956b6fd186fe4
MuhammedMahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2018-02-10 20:31:12 +0200

    Test message.

--- src/main/java/com/talktoki/chatinterfaces/commans/Message.java
+++ src/main/java/com/talktoki/chatinterfaces/commans/Message.java
@@ -11,20 +11,20 @@ import javafx.scene.text.Font;
 public class Message implements Serializable{
 
     private String text;
-    private transient Font font;
-    private transient Color textColor;
-    private boolean isBold, isItalic;
+//    private transient Font font;
+//    private transient Color textColor;
+//    private boolean isBold, isItalic;
 
     public Message() {
     }
 
-    public Message(String text, Font font, Color textColor, boolean isBold, boolean isItalic) {
-        this.text = text;
-        this.font = font;
-        this.textColor = textColor;
-        this.isBold = isBold;
-        this.isItalic = isItalic;
-    }
+//    public Message(String text, Font font, Color textColor, boolean isBold, boolean isItalic) {
+//        this.text = text;
+//        this.font = font;
+//        this.textColor = textColor;
+//        this.isBold = isBold;
+//        this.isItalic = isItalic;
+//    }
 
     /**
      * sets message text for any message
@@ -44,76 +44,76 @@ public class Message implements Serializable{
         return text;
     }
 
-    /**
-     * sets font for that message
-     *
-     * @param font
-     */
-    public void setFont(Font font) {
-        this.font = font;
-    }
-
-    /**
-     * gets font of the message
-     *
-     * @return
-     */
-    public Font getFont() {
-        return font;
-    }
-
-    /**
-     * sets text color of a message
-     *
-     * @param color
-     */
-    public void setTextColor(Color color) {
-        textColor = color;
-    }
-
-    /**
-     * gets text color of the message
-     *
-     * @return text color
-     */
-    public Color getTextColor() {
-        return textColor;
-    }
-
-    /**
-     * sets bold attribute
-     *
-     * @param bold
-     */
-    public void setBold(boolean bold) {
-        isBold = bold;
-    }
-
-    /**
-     * gets boold attribute
-     *
-     * @return true if message text is bold or false other wise
-     */
-    public boolean isBold() {
-        return isBold;
-    }
-
-    /**
-     * sets Italic attribute
-     *
-     * @param italic
-     */
-    public void setItalic(boolean italic) {
-        isItalic = italic;
-    }
-
-    /**
-     * gets Italic attribute
-     *
-     * @return true if message text is bold or false other wise
-     */
-    public boolean isItalic() {
-        return isItalic;
-    }
+//    /**
+//     * sets font for that message
+//     *
+//     * @param font
+//     */
+//    public void setFont(Font font) {
+//        this.font = font;
+//    }
+//
+//    /**
+//     * gets font of the message
+//     *
+//     * @return
+//     */
+//    public Font getFont() {
+//        return font;
+//    }
+//
+//    /**
+//     * sets text color of a message
+//     *
+//     * @param color
+//     */
+//    public void setTextColor(Color color) {
+//        textColor = color;
+//    }
+//
+//    /**
+//     * gets text color of the message
+//     *
+//     * @return text color
+//     */
+//    public Color getTextColor() {
+//        return textColor;
+//    }
+//
+//    /**
+//     * sets bold attribute
+//     *
+//     * @param bold
+//     */
+//    public void setBold(boolean bold) {
+//        isBold = bold;
+//    }
+//
+//    /**
+//     * gets boold attribute
+//     *
+//     * @return true if message text is bold or false other wise
+//     */
+//    public boolean isBold() {
+//        return isBold;
+//    }
+//
+//    /**
+//     * sets Italic attribute
+//     *
+//     * @param italic
+//     */
+//    public void setItalic(boolean italic) {
+//        isItalic = italic;
+//    }
+//
+//    /**
+//     * gets Italic attribute
+//     *
+//     * @return true if message text is bold or false other wise
+//     */
+//    public boolean isItalic() {
+//        return isItalic;
+//    }
 
 }
