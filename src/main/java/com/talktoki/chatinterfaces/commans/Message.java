b3937d298bb85f9152ec3723f135c89f29cccc6f
DESKTOP-AP2CQ3B\Mohamed Mahrou <mmrwwe@gmail.com>
2018-02-10 23:13:34 +0200

    Put Message class back to the required state

--- src/main/java/com/talktoki/chatinterfaces/commans/Message.java
+++ src/main/java/com/talktoki/chatinterfaces/commans/Message.java
@@ -1,6 +1,8 @@
 package com.talktoki.chatinterfaces.commans;
 
 import java.io.Serializable;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  *
@@ -9,21 +11,21 @@ import java.io.Serializable;
 public class Message implements Serializable {
 
     private String text;
-//    private transient Font font;
-//    private transient Color textColor;
-//    private boolean isBold, isItalic;
+    private Font font;
+    private Color textColor;
+    private boolean isBold, isItalic;
 
     public Message() {
         text = "";
     }
 
-//    public Message(String text, Font font, Color textColor, boolean isBold, boolean isItalic) {
-//        this.text = text;
-//        this.font = font;
-//        this.textColor = textColor;
-//        this.isBold = isBold;
-//        this.isItalic = isItalic;
-//    }
+    public Message(String text, Font font, Color textColor, boolean isBold, boolean isItalic) {
+        this.text = text;
+        this.font = font;
+        this.textColor = textColor;
+        this.isBold = isBold;
+        this.isItalic = isItalic;
+    }
     /**
      * sets message text for any message
      *
@@ -42,75 +44,75 @@ public class Message implements Serializable {
         return text;
     }
 
-//    /**
-//     * sets font for that message
-//     *
-//     * @param font
-//     */
-//    public void setFont(Font font) {
-//        this.font = font;
-//    }
-//
-//    /**
-//     * gets font of the message
-//     *
-//     * @return
-//     */
-//    public Font getFont() {
-//        return font;
-//    }
-//
-//    /**
-//     * sets text color of a message
-//     *
-//     * @param color
-//     */
-//    public void setTextColor(Color color) {
-//        textColor = color;
-//    }
-//
-//    /**
-//     * gets text color of the message
-//     *
-//     * @return text color
-//     */
-//    public Color getTextColor() {
-//        return textColor;
-//    }
-//
-//    /**
-//     * sets bold attribute
-//     *
-//     * @param bold
-//     */
-//    public void setBold(boolean bold) {
-//        isBold = bold;
-//    }
-//
-//    /**
-//     * gets boold attribute
-//     *
-//     * @return true if message text is bold or false other wise
-//     */
-//    public boolean isBold() {
-//        return isBold;
-//    }
-//
-//    /**
-//     * sets Italic attribute
-//     *
-//     * @param italic
-//     */
-//    public void setItalic(boolean italic) {
-//        isItalic = italic;
-//    }
-//
-//    /**
-//     * gets Italic attribute
-//     *
-//     * @return true if message text is bold or false other wise
-//     */
-//    public boolean isItalic() {
-//        return isItalic;
-//    }
+    /**
+     * sets font for that message
+     *
+     * @param font
+     */
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    /**
+     * gets font of the message
+     *
+     * @return
+     */
+    public Font getFont() {
+        return font;
+    }
+
+    /**
+     * sets text color of a message
+     *
+     * @param color
+     */
+    public void setTextColor(Color color) {
+        textColor = color;
+    }
+
+    /**
+     * gets text color of the message
+     *
+     * @return text color
+     */
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    /**
+     * sets bold attribute
+     *
+     * @param bold
+     */
+    public void setBold(boolean bold) {
+        isBold = bold;
+    }
+
+    /**
+     * gets boold attribute
+     *
+     * @return true if message text is bold or false other wise
+     */
+    public boolean isBold() {
+        return isBold;
+    }
+
+    /**
+     * sets Italic attribute
+     *
+     * @param italic
+     */
+    public void setItalic(boolean italic) {
+        isItalic = italic;
+    }
+
+    /**
+     * gets Italic attribute
+     *
+     * @return true if message text is bold or false other wise
+     */
+    public boolean isItalic() {
+        return isItalic;
+    }
 }
